c96272e9f7a210cfed45b70628431afaaadad509
Achinta27 <achinta27sep1999@gmail.com>
2025-03-12 11:52:28 +0530

    modified

--- src/components/productpage/OurSimilerProduct.jsx
+++ src/components/productpage/OurSimilerProduct.jsx
@@ -1,53 +1,12 @@
 import React, { useEffect, useState } from "react";
 import ProductDesignCard from "../card/ProductDesignCard";
 import Slider from "react-slick";
+import Link from "next/link";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa6";
 
-const OurSimilerProduct = () => {
+const OurSimilerProduct = ({ products }) => {
   const [slidesToShow, setSlidesToShow] = useState(5);
   const [autoplay, setAutoplay] = useState(true);
-
-  const products = [
-    {
-      productimg: "/images/productring.png",
-      producthoverimg: "/images/producthoverimg.png",
-      productname: "14KT Yellow Gold Gorgeous",
-      productprice: "12599",
-      productMRP: "16599",
-      productDiscount: "20",
-    },
-    {
-      productimg: "/images/productring.png",
-      producthoverimg: "/images/producthoverimg.png",
-      productname: "14KT Yellow Gold Gorgeous",
-      productprice: "12599",
-      productMRP: "16599",
-      productDiscount: "20",
-    },
-    {
-      productimg: "/images/productring.png",
-      producthoverimg: "/images/producthoverimg.png",
-      productname: "14KT Yellow Gold Gorgeous",
-      productprice: "12599",
-      productMRP: "16599",
-      productDiscount: "20",
-    },
-    {
-      productimg: "/images/productring.png",
-      producthoverimg: "/images/producthoverimg.png",
-      productname: "14KT Yellow Gold Gorgeous",
-      productprice: "12599",
-      productMRP: "16599",
-      productDiscount: "20",
-    },
-    {
-      productimg: "/images/productring.png",
-      producthoverimg: "/images/producthoverimg.png",
-      productname: "14KT Yellow Gold Gorgeous",
-      productprice: "12599",
-      productMRP: "16599",
-      productDiscount: "20",
-    },
-  ];
 
   useEffect(() => {
     const handleResize = () => {
@@ -67,7 +26,7 @@ const OurSimilerProduct = () => {
         setSlidesToShow(4);
         setAutoplay(false);
       } else if (window.innerWidth <= 1780) {
-        setSlidesToShow(4);
+        setSlidesToShow(5);
         setAutoplay(false);
       } else if (window.innerWidth <= 2580) {
         setSlidesToShow(5);
@@ -86,6 +45,30 @@ const OurSimilerProduct = () => {
       window.removeEventListener("resize", handleResize);
     };
   }, []);
+
+  const PrevArrow = (props) => {
+    const { onClick } = props;
+    return (
+      <button
+        onClick={onClick}
+        className="absolute bottom-[-1.5rem] right-12 transform -translate-y-1/2 z-10  size-8 bg-custom-green  flex justify-center items-center"
+      >
+        <FaChevronLeft className="text-white size-4" />
+      </button>
+    );
+  };
+
+  const NextArrow = (props) => {
+    const { onClick } = props;
+    return (
+      <button
+        onClick={onClick}
+        className="absolute bottom-[-1.5rem] right-4 transform -translate-y-1/2 z-10 size-8 bg-custom-green  flex justify-center items-center"
+      >
+        <FaChevronRight className="text-white size-4" />
+      </button>
+    );
+  };
   const settings = {
     infinite: products.length > slidesToShow,
     slidesToShow: slidesToShow,
@@ -93,7 +76,8 @@ const OurSimilerProduct = () => {
     autoplay: true,
     speed: 1000,
     autoplaySpeed: 4000,
-    arrows: false,
+    prevArrow: <PrevArrow />,
+    nextArrow: <NextArrow />,
   };
 
   return (
@@ -104,16 +88,20 @@ const OurSimilerProduct = () => {
             className=" w-full !flex justify-center items-center outline-none py-4"
             key={index}
           >
-            <div className="h-fit w-[95%] relative">
+            <Link
+              href={`/products/${item.slug}`}
+              className="h-fit w-[95%] relative"
+            >
               <ProductDesignCard
-                productimg={item.productimg}
-                producthoverimg={item.producthoverimg}
-                productname={item.productname}
-                productDiscount={item.productDiscount}
-                productprice={item.productprice}
-                productMRP={item.productMRP}
+                productId={item._id}
+                productimg={item.thumbnail_image.secure_url}
+                producthoverimg={item.hoverImage.secure_url}
+                productname={item.title}
+                productDiscount={item.discount}
+                productprice={item.price}
+                productMRP={item.mrp}
               />
-            </div>
+            </Link>
           </div>
         ))}
       </Slider>
--- src/pages/products/[slug].js
+++ src/pages/products/[slug].js
@@ -1,10 +1,11 @@
+import { fetchProductsData } from "@/actions/FetchProducts";
 import { fetchSingleProductData } from "@/actions/FetchSingleProduct";
 import SubPageBanner from "@/components/global/SubPageBanner";
 import ProductDetailsPage from "@/components/productpage/ProductDetailsPage";
 import MainPageTemplate from "@/templates/MainPageTemplate";
 import React from "react";
 
-const SingleProductPage = ({ product }) => {
+const SingleProductPage = ({ product, products }) => {
   if (!product) {
     return <div>Product not found</div>;
   }
@@ -28,6 +29,9 @@ const SingleProductPage = ({ product }) => {
         slug={product.slug}
         description={product.description}
         specification={product.specification}
+        stock={product.in_stock}
+        products={products}
+        category={product.category.mainCategory}
       />
     </MainPageTemplate>
   );
@@ -38,11 +42,13 @@ export default SingleProductPage;
 export async function getServerSideProps(context) {
   const { slug } = context.params;
   const product = await fetchSingleProductData(slug);
-  console.log("Fetched product:", product);
+  const category = product.category.mainCategory;
+  const initialData = await fetchProductsData(1, 12, category);
 
   return {
     props: {
       product,
+      products: initialData.products,
     },
   };
 }
